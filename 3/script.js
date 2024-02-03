6fe2c14bd7465437b05f4fce9a7fc05e1e21d0ab
aisezimm00 <150120243+aisezimm00@users.noreply.github.com>
2024-02-03 16:24:32 +0600

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,374 @@
+const array = {
+  name: "aisezim",
+  surname: "bekmamatova",
+  middleName: "chingizovna",
+  school: "olimp",
+  zz: "cancer",
+  family: {
+    dad: {
+      name: "chyngyz",
+      age: 43,
+      address: "ashimahunova",
+      zz: "cancer",
+    },
+    mom: {
+      name: "gulzada",
+      age: 40,
+      address: "ashimahunova",
+      zz: "taurus",
+    },
+    sister: {
+      name: "sumaya",
+      age: 11,
+      address: "ashimahunova",
+      zz: "fish",
+      hobby: "kushatmozgi",
+    },
+    sister: {
+      name: "aidai",
+      age: 6,
+      address: "ashimahunova",
+      zz: "taurus",
+      hobby: "dengiKlyanchit",
+    },
+    brother: {
+      name: "emirhan",
+      age: 10,
+      address: "ashimahunova",
+      zz: "vesy",
+    },
+  },
+  friends: {
+    amina: {
+      name: "amina",
+      school: "Olimp",
+      age: 16,
+      family: {
+        mom: "ziyagil",
+        dad: "mukhtarali",
+        brother: "beka",
+        sister: "shakira",
+        sister: "kamila",
+        brother: "abdullah",
+      },
+    },
+    mirim: {
+      name: "mirim",
+      school: "olimp",
+      age: "15",
+      family: {
+        mom: "nurkyz",
+        dad: "emil",
+        brother: "bayaz",
+        brother: "bayaman",
+        sister: "medina",
+      },
+    },
+    aisezim: {
+      name: "aisezim",
+      school: "zaochka",
+      age: 16,
+      family: {
+        mom: "chynara",
+        dad: "ulan",
+        sister: "zhyldyz",
+        sister: "rayana",
+        sister: "ailin",
+        brother: "emir",
+      },
+    },
+    beka: {
+      name: "bektursun",
+      college: "auca",
+      age: "17",
+      family: {
+        mom: "ziera",
+        dad: "alim",
+        sister: "aybiyke",
+        brother: "umar",
+      },
+    },
+    milana: {
+      name: "milana",
+      school: "zaochka",
+      age: 17,
+      family: {
+        mom: "aida",
+        dad: "bakyt",
+        brother: "bayir",
+        brother: "atai",
+      },
+    },
+    daniel: {
+      name: "daniel",
+      college: "oshMU",
+      age: 17,
+      family: {
+        mom: "anara",
+        dad: "bek",
+        sister: "bermet",
+        brother: "artur",
+      },
+    },
+  },
+  wardrobe: {
+    cardigan: {
+      color: "white",
+      size: "L",
+      price: 2500,
+      brand: "noname",
+    },
+    turtleneck: {
+      color: "ivory",
+      size: "M",
+      price: 1550,
+      brand: "uniqo",
+    },
+    jumper: {
+      color: "navy",
+      size: "standart",
+      price: 3200,
+      brand: "fakePolo",
+    },
+    jacket: {
+      color: "black",
+      size: "L",
+      price: 4200,
+      brand: "massimoduti",
+    },
+    jacket: {
+      color: "black",
+      size: "M",
+      price: 5000,
+      brand: "mango",
+    },
+    longsleeve: {
+      color: "black and whit strip",
+      size: "standart",
+      price: 1900,
+      brand: "noname",
+    },
+    tShirt: {
+      color: "white",
+      size: "L",
+      price: 1500,
+      brand: "uniqlo",
+    },
+    jacket: {
+      color: "beige",
+      size: "standart",
+      price: 4200,
+      brand: "noname",
+    },
+    longsleeve: {
+      color: "pistachio",
+      size: "s",
+      price: 1500,
+      brand: "YYshop",
+    },
+    jeans: {
+      color: "blue",
+      size: "standart",
+      price: 2000,
+      brand: "noname",
+    },
+    socks: {
+      color: "blue",
+      size: "M",
+      price: 150,
+      brand: "sockCo",
+    },
+    dress: {
+      color: "red",
+      size: "S",
+      price: 2800,
+      brand: "fashionista",
+    },
+
+    jeans: {
+      color: "denim",
+      size: "32",
+      price: 1800,
+      brand: "casualChic",
+    },
+
+    hat: {
+      color: "black",
+      size: "One Size",
+      price: 500,
+      brand: "styleTrend",
+    },
+
+    sneakers: {
+      color: "white",
+      size: "8",
+      price: 2000,
+      brand: "sportyComfort",
+    },
+
+    blouse: {
+      color: "pink",
+      size: "L",
+      price: 1200,
+      brand: "eleganceStyle",
+    },
+
+    shorts: {
+      color: "khaki",
+      size: "M",
+      price: 800,
+      brand: "activeLife",
+    },
+
+    sweater: {
+      color: "gray",
+      size: "XL",
+      price: 2200,
+      brand: "warmKnits",
+    },
+
+    scarf: {
+      color: "green",
+      size: "One Size",
+      price: 300,
+      brand: "cozyWrap",
+    },
+
+    blazer: {
+      color: "navy",
+      size: "M",
+      price: 3500,
+      brand: "businessChic",
+    },
+  },
+  music: {
+    song1: {
+      name: "Heartbeat",
+      author: "childish Gambino",
+    },
+    song2: {
+      name: "infrunami",
+      author: "Stive Lacy",
+    },
+    song4: {
+      name: "asylym",
+      author: "prince",
+    },
+    song5: {
+      name: "Is There Someone Else",
+      author: "the weeknd",
+    },
+    song6: {
+      name: "friends",
+      author: "chase atlantics",
+    },
+    song7: {
+      name: "often",
+      author: "the weeknd",
+    },
+    song8: {
+      name: "lost in the fire",
+      author: "the weeknd",
+    },
+    song9: {
+      name: "hotel",
+      author: "",
+    },
+  },
+  series: {
+    TheStrangerThings: {
+      name: "Очень странные дела",
+      favorites: "стив ",
+      rating: 5,
+    },
+    howToGetAwayWithMurder: {
+      name: "как избежать наказания за убийство",
+      favorites: "фрэнк",
+      rating: 4.6,
+    },
+    gossipGirl: {
+      name: "сплетница",
+      favorites: "blair",
+      rating: 5,
+    },
+    walkers: {
+      name: "ходячие мертвецы",
+      favorites: "гленн",
+      rating: 3,
+    },
+    annWith2n: {
+      name: "энн с двумя н",
+      favorites: "ann",
+      rating: 3,
+    },
+    lookingForAlaska: {
+      name: "в поисках аляски",
+      favorites: "аляска",
+      rating: 5,
+    },
+    kuhnya: {
+      name: "кухня",
+      favorites: "виктор",
+      rating: 5,
+    },
+    gossipGirl: {
+      name: "Сплетница",
+      favorites: "Blair",
+      rating: 5,
+    },
+
+    sciFiAdventure: {
+      name: "Галактическая Одиссея",
+      favorites: "Капитан Нова",
+      rating: 4.5,
+    },
+
+    mysteryDetective: {
+      name: "Файлы Загадок",
+      favorites: "Детектив Смит",
+      rating: 4,
+    },
+
+    fantasyEpic: {
+      name: "Царство Легенд",
+      favorites: "Элара",
+      rating: 4.8,
+    },
+
+    romanticComedy: {
+      name: "Любовь в большом Городе",
+      favorites: "Эмма",
+      rating: 4.2,
+    },
+
+    historicalDrama: {
+      name: "Хроники Династии",
+      favorites: "Леди Элеонора",
+      rating: 4.6,
+    },
+
+    superheroAction: {
+      name: "Герои Инферно",
+      favorites: "Файерсторм",
+      rating: 4.7,
+    },
+
+    animatedFantasy: {
+      name: "Капризное Чудо",
+      favorites: "Фея Дрим",
+      rating: 4.3,
+    },
+
+    thrillerMystery: {
+      name: "Полуночная Загадка",
+      favorites: "Агент Нуар",
+      rating: 4.5,
+    },
+
+    comedyAdventure: {
+      name: "Экспресс Смеха",
+      favorites: "Чаклз",
+      rating: 4.1,
+    },
+  },
+};
